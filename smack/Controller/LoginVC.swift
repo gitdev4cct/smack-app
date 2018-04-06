73e937966d373a91ce21c5e99102934dcfdb2133
Kraken <kraken@Krakens-Mac.local>
2018-04-06 15:04:20 -0700

    Added Login UI elements

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Kraken on 4/6/18.
+//  Copyright © 2018 Kraken. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Kraken on 4/6/18.
+//  Copyright © 2018 Kraken. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+let TO_LOGIN = "toLogin"
+
